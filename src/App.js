9e7582c6bf13f7c2a45d3d24d1a3aa38fae5df4d
Mike Mackenzie <mike@diastorm.com>
2018-01-18 13:53:13 +1300

    first commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      data: []
+    }
+  }
+
+  componentDidMount() {
+    let url = "/_designs/nested_assets/new-dcc-design-nested-assets/dcc-v7-refresh-nested-assets/content-via-ajax/road-cond-temp-json"
+    fetch(url)
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({ data: responseJson.results })
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Dunedin Weather</h1>
+        </header>
+        <ul>
+        {this.state.data.map((value, key) => {
+          //console.log(value.attributes)
+          return <li key={key}>{value.attributes.Location} is {value.attributes.Air_Temp}°</li>
+        })}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
